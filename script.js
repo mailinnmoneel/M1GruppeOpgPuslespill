7d1bd4c48e40cd321115498b12f4da59aa452c42
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-05-26 18:30:41 +0200

    ditt og datt

--- script.js
+++ script.js
@@ -7,72 +7,101 @@ var boxHeight = 600;
 var puzzleBox;
 var puzzleBoxWidth;
 
+var info;
+
+var puzzlePieces = [];
+var _1_1;
+var _1_2;
 
 // Setter opp variabler og henter referanse til elementer
 function init()
 {
-    
+    document.addEventListener('mousemove', getMousePosition, true);
 
+    info = document.getElementById("info");
     puzzleBox = document.getElementById("brikkeboks");
+
     puzzleBox.style.width = getPixels(boxWidth);
     puzzleBox.style.height = getPixels(boxHeight);
 
-    console.log("Grid is " + puzzleBox.style.width + " wide");
-    console.log("Grid is " + puzzleBox.style.height + " high");
-
-    document.addEventListener('mousemove', getMousePos, true);
+    // Referanse til alle elementene. Kan gjøres bedre men tar det kjapt for å kunne teste
+    _1_1 = document.getElementById("1-1");   
+    _1_2 = document.getElementById("1-2");
+    
+    _1_1 = new PuzzlePiece("1-1", 100,50);
+    _1_2 = new PuzzlePiece("1-2", 320, 50);
+    
+    puzzlePieces.push(_1_1);
+    puzzlePieces.push(_1_2);
 
     isHoldingObject = false;
 
     setInterval(updateItemPos, 10);
 }
 
-
 function updateItemPos()
 {
+    // Avslutt om vi ikke holder et bilde
+    if (heldObject == null)
+    {
+        info.innerHTML = "Not holding anything"; 
+        return;
+    }     
+
+    
     if (heldObject != null)
     {
-        setPosition(heldObject.obj, new Point(mousePosition.x, mousePosition.y));
+        /* KUN FOR TESTING */
+        info.innerHTML = "Holding " + heldObject.obj.id + "At Object Position X: " + heldObject.x + " Y: " + heldObject.y;        
+        info.innerHTML += "  Mouse Position X: " + mousePosition.x + " Y: " + mousePosition.y;
+        /* KUN FOR TESTING */
+
+        heldObject.setPosition(mousePosition.x, mousePosition.y)
+        setPosition(heldObject.obj, heldObject.x, heldObject.y);
     }
 }
 
 // Plukker opp brikke
 async function pickUpPiece(_clickedID)
-{
-    await sleep(200);
+{  
 
-    if (isHoldingObject == false && _clickedID != puzzleBox.id)
-    {        
-        isHoldingObject = true;
-        heldObject = new HeldObject(_clickedID, mousePosition.x, mousePosition.y);
-        console.log(heldObject.obj);
-    }
-}
-
-async function dropPiece()
-{    
-    if (isHoldingObject == true)
-    {   
-        await sleep(200);    
-
-        setPosition(heldObject.obj, new Point(mousePosition.x, mousePosition.y));
-        //heldObject = null;
-        isHoldingObject = false;
-    }  
-}
-
-function setPosition(_obj, _pos)
-{
-    _obj.style.left = getPixels(_pos.x);
-    _obj.style.top = getPixels(_pos.y);
-}
-
-// Henter museposisjon
-function getMousePos(e)
-{
     if (isHoldingObject)
         return;
 
+    await sleep(20);
+
+    if (_clickedID != puzzleBox.id)
+    {        
+        isHoldingObject = true;
+        for (p = 0; p < puzzlePieces.length; p++)
+        {
+            if (puzzlePieces[p].obj.id == _clickedID)
+            {
+                heldObject = puzzlePieces[p].obj;
+            }
+        }
+    }
+}
+
+function dropPiece()
+{
+    if (isHoldingObject == false)
+        return;   
+    
+    console.log("Dropped picture number " + heldObject.obj.id);
+    heldObject = null;
+    isHoldingObject = false; 
+}
+
+function setPosition(_obj, _x, _y)
+{
+    _obj.style.left = getPixels(_x-heldObject.offsetx);
+    _obj.style.top = getPixels(_y-heldObject.offsety);
+}
+
+
+function getMousePosition(e)
+{
     // Vet ikke hva denne gjør
     e.preventDefault();
 
@@ -91,23 +120,29 @@ function getPixels(coord)
 }
 
 
-class HeldObject
+class PuzzlePiece
 {  
     constructor(_id, _x, _y)
     {
-        this.pos = new Point(_x, _y);
+        this.x = _x;
+        this.y = _y;
+
         this.obj = document.getElementById(_id);
+        this.obj.style.left = getPixels(this.x);
+        this.obj.style.top = getPixels(this.y);
     }
 }
 
-class Point
+
+
+
+function sleep(ms)
 {
-    constructor(_x, _y)
-    {
-        this.x = _x;
-        this.y = _y;
-    }
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+
 
 
 
@@ -128,9 +163,3 @@ class Point
 //     div.style.left = x.toString() + "px";
 //     div.style.top = y.toString() + "px";
 // }
-
-
-function sleep(ms)
-{
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
